31b92091493eed91a6606615ab852420e4c0c848
hamiciryadh <hamiciryadh@gmail.com>
2021-01-04 14:41:14 +0100

    another test

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) {
         if (args.length < 2) {
             System.err.println("Too few arguments, the correct syntax is : <ip address> <port>");
+            System.err.println();
             System.exit(1);
         }
 
