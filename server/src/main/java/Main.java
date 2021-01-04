733f30c85cf7d7a37094ec30a1d9b1e45b7973c8
HAMICI Ryadh <hamiciryadh@gmail.com>
2021-01-04 19:19:32 +0100

    Master ryadh (#9)
    * somehing or other
    
    * Master2 (#1)
    
    * Removing useless files
    
    * Removing useless files
    
    * Nothing much
    
    * added decoration
    
    * another test
    
    * else
    
    Co-authored-by: hamiciryadh <hamiciryadh@gmail.com>
    
    * test
    
    * deleting (#2)
    
    Co-authored-by: hamiciryadh <hamiciryadh@gmail.com>
    
    * Deleting message (#3)
    
    * Removing useless files
    
    * Removing useless files
    
    * Nothing much
    
    * added decoration
    
    * another test
    
    * deleting
    
    Co-authored-by: hamiciryadh <hamiciryadh@gmail.com>
    
    * deleting
    
    * deleting
    
    * From eclipse
    
    * add
    
    * .3.
    
    * altering
    
    Co-authored-by: temporary-something <76949881+temporary-something@users.noreply.github.com>

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -8,7 +8,8 @@ public class Main {
     public static void main(String[] args) {
         if (args.length < 2) {
             System.err.println("Too few arguments, the correct syntax is : <ip address> <port> ...");
-            System.out.println("321");
+            System.out.println("Eclipse");
+            System.out.println("123321");
             System.exit(1);
         }
 
